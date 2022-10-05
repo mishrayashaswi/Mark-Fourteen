b8cee804bc4929f0047499d77e827de4f6eeb2ee
Yahaswi Mishra <59636221+mishrayashaswi@users.noreply.github.com>
2022-10-05 10:51:48 +0530

    feat: final commit for profit/loss calculation  in stocks

---
+++ app.js
@@ -0,0 +1,35 @@
+var initialPrice = document.querySelector("#initial");
+var Quantity = document.querySelector("#quantity");
+var currentPrice = document.querySelector("#current");
+var submit = document.querySelector("#submit");
+var output = document.querySelector("#output");
+
+submit.addEventListener("click", submitHandler);
+
+function submitHandler() {
+  var ip = Number(initialPrice.value);
+  var qty = Number(Quantity.value);
+  var cur = Number(currentPrice.value);
+
+  calculate(ip, qty, cur);
+}
+
+function calculate(initial, quantity, current) {
+  if (initial > current) {
+    var loss = (initial - current) * quantity;
+    var lossPer = (loss / initial) * 100;
+
+    outputShow("Ttal loss is " + loss + "₹ and Loss percentage " + lossPer + " %");
+  } else if (current > initial) {
+    var profit = (current - initial) * quantity;
+    var profitPer = (profit / initial) * 100;
+
+    outputShow("Total profit is " + profit + "₹ and Profit percentage " + profitPer + " %");
+  } else if(current = initial)
+  {
+    outputShow("No Profit No Loss");
+  }
+}
+function outputShow(message) {
+  output.innerHTML = message;
+}
